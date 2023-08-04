6930ad468f00978be7af2402a781bfe8975199b0
Eduardo Chinchilla <edutrm123@gmail.com>
2023-08-04 14:07:55 -0600

    Segundo commit, las clases de automovil, motocicleta y autobus ya tienen su atributos y su herencia de la clase Vehiculo

--- src/lab3p2_eduardochinchilla/Lab3P2_EduardoChinchilla.java
+++ src/lab3p2_eduardochinchilla/Lab3P2_EduardoChinchilla.java
@@ -6,7 +6,37 @@ public class Lab3P2_EduardoChinchilla {
     
     static Scanner read = new Scanner(System.in);
     public static void main(String[] args) {
+        int opcion = 0;
         
+        while (opcion != 8){
+            
+            opcion = menuCRUD();
+            
+            switch (opcion){
+                case 1:{
+                    break;
+                }
+                case 2:{
+                    break;
+                }
+                case 3:{
+                    break;
+                }
+                case 4:{
+                    break;
+                }
+                case 5:{
+                    break;
+                }
+                case 7:{
+                    break;
+                }
+                case 8:{
+                    System.out.println("Salio");
+                    break;
+                }
+            }
+        }
     
         
         
@@ -20,12 +50,14 @@ public class Lab3P2_EduardoChinchilla {
 
         System.out.println("""
                            Menu CRUD
-                           1- Agregar Producto
-                           2- Modificar Producto
-                           3- Eliminar Producto
-                           4- Simulacion de Facturacion
-                           5- Reporte
-                           6- Salir
+                           1- Agregar Automovil
+                           2- Agregar Motocicleta
+                           3- Agregar Autobus
+                           4- Modificar vehiculo
+                           5- Eliminar vehiculo
+                           6- Mostar Vehiculo
+                           7- Generar Boleta
+                           8- Salir
                            """);
 
         op = read.nextInt();
